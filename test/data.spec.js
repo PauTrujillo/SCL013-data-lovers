b50afc9496ca30ec275e0181fb722da2041afb28
PauTrujillo <pautrujillomorales@gmail.com>
2020-05-14 11:48:24 -0400

    empezando a entender filtros

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,30 @@
-import { example, anotherExample } from '../src/data.js';
+import {filterData} from '../src/data.js';
+
+
+describe('filterData', () => {
+  test('is a function', () => {
+    expect(typeof filterData).toBe('function');
+  });
+
+  test('should return "male" for filter "type"', () => {
+    expect(example()).toBe('example');
+  });
+});
+
+
+describe('anotherExample', () => {
+  test('is a function', () => {
+    expect(typeof anotherExample).toBe('function');
+  });
+
+  test('returns `anotherExample`', () => {
+    expect(anotherExample()).toBe('OMG');
+  });
+});
+
+
+
+/*import { example, anotherExample } from '../src/data.js';
 
 
 describe('example', () => {
@@ -20,4 +46,4 @@ describe('anotherExample', () => {
   test('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
   });
-});
+});*/
